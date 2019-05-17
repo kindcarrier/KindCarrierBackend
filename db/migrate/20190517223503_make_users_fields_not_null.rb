16eaf63936f0f87b6a068d6df31a0abe1d8295be
TolichP <alexandr.privorotskiy@yandex.ru>
2019-05-18 06:11:22 +0700

    Make user fields not nullable

---
+++ db/migrate/20190517223503_make_users_fields_not_null.rb
@@ -0,0 +1,27 @@
+class MakeUsersFieldsNotNull < ActiveRecord::Migration[5.2]
+  def up
+    change_table(:users, bulk: true) do |t|
+      t.change :first_name, :string, null: false
+      t.change :last_name, :string, null: false
+      t.change :email, :string, null: false
+      t.change :crypted_password, :string, null: false
+      t.change :salt, :string, null: false
+      t.change :token, :string, null: false
+
+      t.index :email, unique: true
+    end
+  end
+
+  def down
+    change_table(:users, bulk: true) do |t|
+      t.change :first_name, :string, null: true
+      t.change :last_name, :string, null: true
+      t.change :email, :string, null: true
+      t.change :crypted_password, :string, null: true
+      t.change :salt, :string, null: true
+      t.change :token, :string, null: true
+
+      t.remove_index :email
+    end
+  end
+end
