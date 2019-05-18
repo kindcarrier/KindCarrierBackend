8125e9d0b39f4301a2c087bdb1639f22be1045d7
Dmitry Kazansky <dmitriy95@bk.ru>
2019-05-18 19:15:35 +0700

    fix test

--- spec/factories/deal.rb
+++ spec/factories/deal.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :deal do
     description { 'deal_description' }
     supplying_cost { rand(20..30) }
-    negotiation_ids { [create(:negotiation, type: 'recipient').id, 
-                        create(:negotiation, type: 'deliveryman').id] }
+    negotiation_ids { [create(:negotiation, type: 'recipient').id, create(:negotiation, type: 'deliveryman').id] }
 end
