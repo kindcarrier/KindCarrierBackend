fc3120b9836e58516b3ade5213161f654afdff90
TolichP <alexandr.privorotskiy@yandex.ru>
2019-05-19 11:11:35 +0700

    Move swagger to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'index#index'
+  mount Rswag::Ui::Engine => '/'
+  mount Rswag::Api::Engine => '/'
+  
   resources :users, only: %i[index create show update]
   resources :negotiations, only: %i[index create show]
   resources :deals, only: %i[index create show]
   resource :session, onle: [:create]
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
 end
